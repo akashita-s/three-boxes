7f4d24682e701608cc5b81081634c1686bfd9b46
akashita-s <82257553+akashita-s@users.noreply.github.com>
2021-11-29 17:55:02 +0530

    Packages and Navigation Bar setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { AppBar, Typography, Toolbar, Drawer } from "@material-ui/core";
+import { useState } from "react";
+import { Canvas } from "@react-three/fiber";
 
 function App() {
+  const Box = (props) => {
+    return (
+      <mesh {...props}>
+        <boxGeometry />
+        <meshNormalMaterial color="blue" />
+      </mesh>
+    );
+  };
+
+  const [box, setBox] = useState("Box 1");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar position="sticky" color="#fff">
+        <Toolbar style={{ display: "flex", justifyContent: "center" }}>
+          <Typography variant="h6">{box}</Typography>
+        </Toolbar>
+      </AppBar>
+      <div
+        style={{
+          height: "80vh",
+          diplay: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: "300px",
+        }}
+      >
+        <Canvas>
+          <Box
+            position={[2, 2, 0]}
+            onClick={() => {
+              setBox("Box 2");
+            }}
+          />
+          <Box
+            position={[-2, 2, 0]}
+            onClick={() => {
+              setBox("Box 1");
+            }}
+          />
+        </Canvas>
+      </div>
+     
     </div>
   );
 }
